bc3af0610e4abc33572c063902fc6a081fb13521
nazim1997 <nazimberkane97@gmail.com>
2025-03-06 16:05:27 +0100

    -       Correction of writer.c file

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -15,30 +15,31 @@ int main(int argc, char *argv[])
     if (argc != 3 || strcmp(argv[1], "--help") == 0) {
         printf("you should provide two arguments\n");
         syslog(LOG_ERR, "you should provide two arguments");
+        closelog();
         return 1;
     }
 
-    fileFd = open(argv[1], O_WRONLY);
+    fileFd = open(argv[1], O_WRONLY | O_CREAT, 0644);
 
     if (fileFd == -1) {
         printf("error opening file %s\n", argv[1]);
         syslog(LOG_ERR, "error opening file %s", argv[1]);
+        closelog();
         return 1;
     }
-
-    printf("buffer is  %s\n", argv[2]);
 
     numRead = write(fileFd, argv[2], strlen(argv[2]));
 
     if(numRead < 0) {
         printf("error writing to file %s\n", argv[1]);
         syslog(LOG_ERR, "error writing to file %s", argv[1]);
+        close(fileFd);
+        closelog();
         return 1;
     }
 
     syslog(LOG_DEBUG ,  "Writing %s to %s", argv[2], argv[1]);
 
-    printf("numRead is   %ld\n", numRead);
     close(fileFd);
     return 0;
 }
