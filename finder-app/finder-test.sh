f573b4392fa5a8ff6c44c2a9134195dcc11ce6c3
nazim1997 <nazimberkane97@gmail.com>
2025-03-06 14:19:04 +0100

    -       Revert finder-test.sh file

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -30,19 +30,15 @@ MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines a
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
-echo "you are in file : $(pwd)"
 # create $WRITEDIR if not assignment1
 assignment=`cat conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
-	echo "here 0"
 	mkdir -p "$WRITEDIR"
-	echo "here 1"
 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
 	#The quotes signify that the entire string in WRITEDIR is a single string.
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	echo "here 2"
 	if [ -d "$WRITEDIR" ]
 	then
 		echo "$WRITEDIR created"
@@ -57,7 +53,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
