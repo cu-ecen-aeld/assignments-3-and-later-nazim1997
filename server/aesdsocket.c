03841efd54abd3f30d58a0f8d37919e990480d28
nazim1997 <nazimberkane97@gmail.com>
2025-03-22 17:24:40 +0100

    -       Added server folder

---
+++ server/aesdsocket.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX 80 
+
+void listening(int fd) {
+  char buffer[MAX];
+
+  int aesdsocketdata_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND);
+
+  for (;;) {
+    bzero(buffer, MAX);
+
+    read(fd, buffer, strlen(buffer));
+
+    write(aesdsocketdata_fd, buffer, strlen(buffer));
+
+    write(fd, buffer, strlen(buffer));
+  }
+}
+
+int main() {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sockfd == -1) {
+    perror("socket creation failed...\n");
+    exit(1);
+  } 
+
+  struct sockaddr_in server_address, client_address;
+  memset(&server_address, 0, sizeof(server_address));
+
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(9000);
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sockfd, (struct sockaddr )&server_address, sizeof(server_address)) == -1) {
+    perror("binding failed\n");
+    exit(1);
+  }
+
+  if (listen(sockfd, 5) == -1) {
+    perror("listening failed\n");
+    exit(1);
+  }
+
+  socklen_t client_address_length = sizeof(client_address);
+
+  int connfd = accept(sockfd, (struct sockaddr)&client_address, &client_address_length);
+
+  if (connfd < 0) {
+      perror("server accepted failed");
+  }
+  else {
+    syslog(LOG_INFO, "Accepted connection from %d", client_address.sin_addr.s_addr);
+  }
+
+  listening(connfd);
+
+  return 0;
+}
