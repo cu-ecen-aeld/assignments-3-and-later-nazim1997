04f75e1a87a8d1e84147f362c0ebd49d91a980f9
nazim1997 <nazimberkane97@gmail.com>
2025-04-09 23:19:22 +0200

    -       some corrections

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,64 +10,107 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
+#include <signal.h>
+#include <stdbool.h>
 
-#define MAX 80 
+#define MAX 80
+#define PORT 9000
 
-void listening(int fd) {
-  char buffer[MAX];
+volatile sig_atomic_t terminate_flag = false;
 
-  int aesdsocketdata_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND);
-
-  for (;;) {
-    bzero(buffer, MAX);
-
-    while(read(fd, buffer, sizeof(buffer)) > 0) {
-
-        write(aesdsocketdata_fd, buffer, sizeof(buffer));
-
-        write(fd, buffer, sizeof(buffer));
+static void signal_handler(int signal_number) {
+    if (signal_number == SIGINT || signal_number == SIGTERM) {
+        terminate_flag = true;
     }
-  }
+}
+
+void handle_client(int connfd) {
+    char buffer[MAX];
+    int bytes_read;
+    int aesdsocketdata_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND, 0644);
+    
+    if (aesdsocketdata_fd == -1) {
+        perror("open failed");
+        close(connfd);
+        return;
+    }
+
+    while ((bytes_read = read(connfd, buffer, sizeof(buffer))) > 0) {
+        write(aesdsocketdata_fd, buffer, bytes_read);
+        
+        // Check for newline to determine packet completion
+        if (memchr(buffer, '\n', bytes_read) != NULL) {
+            // Send back the complete file content
+            lseek(aesdsocketdata_fd, 0, SEEK_SET);
+            while ((bytes_read = read(aesdsocketdata_fd, buffer, sizeof(buffer))) > 0) {
+                write(connfd, buffer, bytes_read);
+            }
+        }
+    }
+
+    close(aesdsocketdata_fd);
+    close(connfd);
 }
 
 int main() {
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd, connfd;
+    struct sockaddr_in server_address, client_address;
+    socklen_t client_len;
+    struct sigaction sa;
 
-  if (sockfd == -1) {
-    perror("socket creation failed...\n");
-    exit(1);
-  } 
+    // Set up signal handling
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = signal_handler;
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
 
-  struct sockaddr_in server_address, client_address;
-  memset(&server_address, 0, sizeof(server_address));
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(9000);
-  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    // Set up server address
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
 
-  if (bind(sockfd, (struct sockaddr* )&server_address, sizeof(server_address)) == -1) {
-    perror("binding failed\n");
-    exit(1);
-  }
+    // Bind socket
+    if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
-  if (listen(sockfd, 5) == -1) {
-    perror("listening failed\n");
-    exit(1);
-  }
+    // Listen for connections
+    if (listen(sockfd, 5) == -1) {
+        perror("listen failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
-  socklen_t client_address_length = sizeof(client_address);
+    // Main server loop
+    while (!terminate_flag) {
+        client_len = sizeof(client_address);
+        connfd = accept(sockfd, (struct sockaddr *)&client_address, &client_len);
+        
+        if (connfd < 0) {
+            if (terminate_flag) break;
+            perror("accept failed");
+            continue;
+        }
 
-  int connfd = accept(sockfd, (struct sockaddr*)&client_address, &client_address_length);
+        syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(client_address.sin_addr));
+        handle_client(connfd);
+        syslog(LOG_INFO, "Closed connection from %s", inet_ntoa(client_address.sin_addr));
+    }
 
-  if (connfd < 0) {
-      perror("server accepted failed");
-  }
-  else {
-    syslog(LOG_INFO, "Accepted connection from %d", client_address.sin_addr.s_addr);
-  }
-
-  listening(connfd);
-
-  return 0;
+    // Cleanup
+    close(sockfd);
+    unlink("/var/tmp/aesdsocketdata");
+    syslog(LOG_INFO, "Caught signal, exiting");
+    
+    return 0;
 }
