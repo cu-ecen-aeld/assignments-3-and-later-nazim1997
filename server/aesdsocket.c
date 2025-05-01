adcdeeb75435fe8710a040740704d5782436d3d9
nazim1997 <nazimberkane97@gmail.com>
2025-05-01 16:01:59 +0200

    -       attempt

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,11 +12,22 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <sys/queue.h>
 
 #define MAX 80
 #define PORT 9000
 
 volatile sig_atomic_t terminate_flag = false;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int aesdsocketdata_fd = -1;
+
+struct thread_node {
+    pthread_t thread;     // Store thread ID here
+    SLIST_ENTRY(thread_node) entries;  // Macro for list linkage
+};
+SLIST_HEAD(thread_list, thread_node) head;  // Define list head type
 
 static void signal_handler(int signal_number) {
     if (signal_number == SIGINT || signal_number == SIGTERM) {
@@ -24,18 +35,16 @@ static void signal_handler(int signal_number) {
     }
 }
 
-void handle_client(int connfd) {
+void* handle_client(void* args) {
+    int connfd = *((int *) args);
+    free(args);
     char buffer[MAX];
     int bytes_read;
-    int aesdsocketdata_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND, 0644);
     
-    if (aesdsocketdata_fd == -1) {
-        perror("open failed");
-        close(connfd);
-        return;
-    }
-
+    printf("accepted connection from thread %d\n", gettid());
+    
     while ((bytes_read = read(connfd, buffer, sizeof(buffer))) > 0) {
+        pthread_mutex_lock(&mutex);
         write(aesdsocketdata_fd, buffer, bytes_read);
         
         // Check for newline to determine packet completion
@@ -43,28 +52,44 @@ void handle_client(int connfd) {
             // Send back the complete file content
             lseek(aesdsocketdata_fd, 0, SEEK_SET);
             while ((bytes_read = read(aesdsocketdata_fd, buffer, sizeof(buffer))) > 0) {
-                write(connfd, buffer, bytes_read);
+                if (write(connfd, buffer, bytes_read) < 0) {
+                    perror("writing to file failed");
+                    break;
+                }
             }
         }
+        pthread_mutex_unlock(&mutex);
     }
+    
 
     close(aesdsocketdata_fd);
     close(connfd);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char **argv) {
-    int sockfd, connfd;
+    int sockfd;
     struct sockaddr_in server_address, client_address;
     socklen_t client_len;
     struct sigaction sa;
     int daemon_mode = 0;
     int c;
+    pthread_t thread;
+    SLIST_INIT(&head);
 
     // Set up signal handling
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = signal_handler;
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
+
+    aesdsocketdata_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_APPEND, 0644);
+    
+    if (aesdsocketdata_fd == -1) {
+        perror("open failed");
+        close(aesdsocketdata_fd);
+        exit(EXIT_FAILURE);
+    }
 
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -108,6 +133,7 @@ int main(int argc, char **argv) {
       else if (pid == 0) {
         printf("starting daemon process\n");
         setsid();
+        close(STDIN_FILENO);
       }
       else {
         exit(0);
@@ -117,21 +143,44 @@ int main(int argc, char **argv) {
     // Main server loop
     while (!terminate_flag) {
         client_len = sizeof(client_address);
-        connfd = accept(sockfd, (struct sockaddr *)&client_address, &client_len);
+        int* connfd = (int*) malloc(sizeof(int));
+        *connfd = accept(sockfd, (struct sockaddr *)&client_address, &client_len);
         
-        if (connfd < 0) {
+        if (*connfd < 0) {
+            free(connfd);
             if (terminate_flag) break;
             perror("accept failed");
             continue;
         }
 
         syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(client_address.sin_addr));
-        handle_client(connfd);
-        syslog(LOG_INFO, "Closed connection from %s", inet_ntoa(client_address.sin_addr));
+        struct thread_node* new_node = malloc(sizeof(struct thread_node));
+        
+        if (pthread_create(&new_node->thread, NULL, handle_client, (void *)connfd) < 0) {
+            perror("thread creation failed");
+            free(connfd);
+            free(new_node);
+        }
+        pthread_mutex_lock(&mutex);
+        SLIST_INSERT_HEAD(&head, new_node, entries);
+        pthread_mutex_unlock(&mutex);   
     }
 
     // Cleanup
     close(sockfd);
+
+    // Signal all threads to exit
+    struct thread_node *node;
+    while (!SLIST_EMPTY(&head)) {
+        node = SLIST_FIRST(&head);
+        SLIST_REMOVE_HEAD(&head, entries);
+        pthread_join(node->thread, NULL);
+        free(node);
+    }
+
+    if (aesdsocketdata_fd != -1) {
+        close(aesdsocketdata_fd);
+    }
     unlink("/var/tmp/aesdsocketdata");
     syslog(LOG_INFO, "Caught signal, exiting");
     
